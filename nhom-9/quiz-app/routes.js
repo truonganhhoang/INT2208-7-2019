37ad2b20dfa9eee9861e3eb5542ca5314a167df5
namtp12 <namtp12.uet@gmail.com>
2019-03-22 17:28:31 +0700

    Add CORS pkg for connecting with angular, add routes for querying user database

--- nhom-9/quiz-app/routes.js
+++ nhom-9/quiz-app/routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var quizs = require('./Models/Quiz');
+var quizs = require('./models/Quiz');
+var users = require('./models/Users');
 router.get('/', (req, res, next) =>{
     quizs.getAllQuizs(function (err, rows) {
         if (err) {
@@ -12,7 +13,7 @@ router.get('/', (req, res, next) =>{
 })
 router.get('/:id?', function (req, res, next) {
     if (req.params.id) {
-        quizs.getQuizByid(req.params.id, function (err, rows) {
+        quizs.getQuizsByid(req.params.id, function (err, rows) {
             if (err) {
                 res.json(err);
             } else {
@@ -60,4 +61,46 @@ router.put('/:id', function (req, res, next) {
         }
     });
 });
+
+router.get('/api/users', function(req, res) {
+    users.getAllUsers(function (err, rows) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+router.post('/api/users', function (req, res, next) {
+    console.log(req);
+    users.addUser(req.body, function (err, count) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(req.body);
+        }
+    });
+});
+
+router.delete('/api/users', function (req, res, next) {
+    quizs.deleteQuiz(req.params.id, function (err, count) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(count);
+        }
+    });
+});
+
+router.put('/api/users', function (req, res, next) {
+    quizs.updateQuiz(req.params.id, req.body, function (err, rows) {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
 module.exports = router;
