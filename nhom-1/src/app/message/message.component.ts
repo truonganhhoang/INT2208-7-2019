25b5eb7d2dcfc20f855788d857f9d03e217fc360
sockyone <sockyone16899@gmail.com>
2019-03-26 11:41:50 +0700

    chat socket test

--- nhom-1/src/app/message/message.component.ts
+++ nhom-1/src/app/message/message.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Message } from '../_models/message.model';
 
 @Component({
   selector: 'app-message',
@@ -8,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class MessageComponent implements OnInit {
 
   constructor() { }
+
+  @Input() message: Message;
 
   ngOnInit() {
   }
--- nhom-1/src/app/messenger/messenger.component.ts
+++ nhom-1/src/app/messenger/messenger.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as io from 'socket.io-client';
 import { UserService } from '@app/_services';
+import { Message } from '../_models/message.model'
 
 @Component({
   selector: 'app-messenger',
@@ -14,24 +15,37 @@ export class MessengerComponent implements OnInit {
   ) { }
 
   socket:any;
+  messages: Message[] = []; 
 
   ngOnInit() {
-  }
-  
-  connect() {
     this.socket = io('http://localhost:3000');
-    console.log(this.userService.currentUserValue);
-    this.socket.emit('login',{
+    this.socket.emit('join chat room',{
       user1: this.userService.currentUserValue.username,
       user2: 'namphan'
     });
-    this.socket.on('hello',(data)=>{
-      console.log(data);
+
+    //this.socket.join(this.genRoom(this.userService.currentUserValue.username,'namphan'));
+    this.socket.on('new message', (data)=>{
+      let message = new Message(data);
+      this.messages.push(message);
     });
-    
-  }
-  disconnect() {
-    this.socket.disconnect();
   }
 
+  sendMessage($event) {
+    if ($event.target.value=="") return;
+    let data = {
+      sender: this.userService.currentUserValue.username,
+      receiver: 'admin',
+      content: $event.target.value,
+      sendDate: new Date()
+    }
+    this.socket.emit('new message', data);
+    this.messages.push(new Message(data));
+    $event.target.value="";
+  }
+
+  genRoom(username1,username2) {
+    return username1 > username2 ? username1.concat('_'+username2) : username2.concat('_'+username1+'_roomchat');
+  }
+  
 }
