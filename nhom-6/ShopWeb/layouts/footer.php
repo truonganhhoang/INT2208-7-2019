3468f8f1db07ec7e50f20af9e629869ee66bb170
thanhtrunggvc@gmail.com <thanhtrunggvc@gmail.com>
2019-04-23 20:30:36 +0700

    Create footer.php

---
+++ nhom-6/ShopWeb/layouts/footer.php
@@ -0,0 +1,56 @@
+
+<div class="wrap">	
+	<div class="section group">
+		<div class="col_1_of_4 span_1_of_4">
+			<h4>Thông tin</h4>
+			<ul>
+				<li><a href="#">Về chúng tôi</a></li>
+				<li><a href="#">Dịch vụ khách hàng</a></li>
+				<li><a href="#">Điểu khoản sử dụng</a></li>
+			</ul>
+		</div>
+
+		<div class="col_1_of_4 span_1_of_4">
+			<h4>Chính sách</h4>
+			<ul>
+				<li><a href="#">Chính sách bảo mật</a></li>
+				<li><a href="#">Chính sách bảo hành</a></li>
+				<li><a href="#">Chính sách đổi trả</a></li>
+			</ul>
+		</div>
+
+		<div class="col_1_of_4 span_1_of_4">
+			<h4>Tài khoản</h4>
+			<ul>
+				<li><a href="/PHP_ShopWeb/login">Đăng nhập</a></li>
+				<li><a href="#">Xem giỏ hàng</a></li>
+				<li><a href="#">Sản phẩm yêu thích</a></li>
+			</ul>
+		</div>
+
+		<div class="col_1_of_4 span_1_of_4">
+			<h4>Liên hệ</h4>
+			<ul>
+				<li><span>+84 xxx xxxx</span></li>
+				<div class="social-icons">
+					<li><a href="#" target="_blank"><img src="/PHP_ShopWeb/public/home/images/facebook.png" alt="" /></a></li>
+					<li><a href="#" target="_blank"><img src="/PHP_ShopWeb/public/home/images/twitter.png" alt="" /></a></li>
+					<li><a href="#" target="_blank"><img src="/PHP_ShopWeb/public/home/images/skype.png" alt="" /> </a></li>
+				</div>
+			</ul>
+		</div>
+			
+	</div>			
+</div>
+	
+<div class="copy_right">
+	<p>&copy; 2019 N6Shop UET-VNU| Design by UET Team</p>
+</div>
+
+<script type="text/javascript">
+	$(document).ready(function() {			
+		$().UItoTop({ easingType: 'easeOutQuart' });
+	});
+</script>
+
+<a href="#" id="toTop"><span id="toTopHover"> </span></a>  
