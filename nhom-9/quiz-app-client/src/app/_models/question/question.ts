f463a2c886552963f355f252ba204086d8876da4
Trần Đức Trung <38934471+niits@users.noreply.github.com>
2019-04-29 01:10:55 +0700

    Sửa lỗi Nam mô tả rằng không lấy ra được mảng chứa các câu hỏi.
    Nhìn chung đã sửa xong tuy nhiên việc của ngày mai là đổi kiểu trả về là Promise để khắc phục việc hàm update() ở Testing component gọi ra trước.

--- nhom-9/quiz-app-client/src/app/_models/question/question.ts
+++ nhom-9/quiz-app-client/src/app/_models/question/question.ts
@@ -1,4 +1,17 @@
-export class Question  {
+export class Question {
+    constructor(
+        questionContent: string,
+        optionA: string,
+        optionB: string,
+        optionC: string,
+        optionD: string
+    ) {
+        this.questionContent = questionContent;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
+    }
     questionContent: string;
     optionA: string;
     optionB: string;
