a3325790fe1331c10a9ffc98eaf6381ba9b696c0
namtp12 <namtp12.uet@gmail.com>
2019-04-30 21:14:32 +0700

    Add @Input() to pass avatar link from dashboard to user-detail

--- nhom-9/quiz-app-client/src/app/dashboard/user-detail/user-detail.component.ts
+++ nhom-9/quiz-app-client/src/app/dashboard/user-detail/user-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-user-detail',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+
+  @Input() imgLink: string;
 
   constructor() { }
 
