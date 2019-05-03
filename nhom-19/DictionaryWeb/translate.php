db7e1fae060d7d67d5eba9131abb6b1491f7faa7
NguyenHuyenTrang070899 <37436902+NguyenHuyenTrang070899@users.noreply.github.com>
2019-05-03 10:38:49 +0700

    Xây dựng giao diện ban đầu cho Tab Dịch đoạn văn bản

--- nhom-19/DictionaryWeb/translate.php
+++ nhom-19/DictionaryWeb/translate.php
@@ -26,7 +26,6 @@
             background-color: #f8f8f8;
             font-size: 16px;
             resize: vertical;
-	margin-top:70px;
         }
         p {
             width: 100%;
@@ -39,50 +38,21 @@
             font-size: 16px;
             resize: vertical;
             overflow: auto;
-	margin-top:70px;
         }
     </style>
-     <script language="javascript">
-               function translate_ajax()
-                {
-                    $.ajax({
-                        url : "./dich.php", 
-                        type : "get",  
-                        dateType:"text", 
-                        data : { 
-                            word :     $('#word').val(),
-                            source: 	$('#source').val(),
-                            target: 	$('#target').val()
-                        },
-                        success : function (result){
-                        	result=result.replace(new RegExp('\r?\n','g'), '<br />');
-                            $('#result').html(result);
-                            $('.btn').html('Translate');
-                        }
-                    });
-                } ;
-                function swap() {
-				    if (document.getElementById("source").value != "")
-				    {
-				        var b = document.getElementById("source").value;
-				        var a = document.getElementById("target").value;
-				        document.getElementById("source").value = a;
-				        document.getElementById("target").value = b;			        
-				    }
-				}
-        </script>    
+
 </head>
 <body>
 <!-- Begin: HEADER -->
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark nav-justified nav-pills" role="navigation">
-<a class="navbar-brand" href="./translate.php"><img src="./img/icon.png"></a>
+<a class="navbar-brand" href="./translate.php">Dictionary</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
 </button>
 <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav mr-auto nav-pills">
         <li class="nav-item">
-            <a class="nav-link disabled" href="./index.php" style="margin-left:130px">Tra từ online</a>
+            <a class="nav-link disabled" href="./index.php">Tra từ online</a>
         </li>
 	</ul>
     <ul class="navbar-nav mr-auto nav-pills">
@@ -98,17 +68,16 @@
 </div>
 </nav>
 <!-- END HEADER -->
-  
+
 <div class="container">
   <div class="row">
     <div class="col-sm-6">
       	<textarea type="text" name="word" id="word" placeholder="Nội dung cần dịch"></textarea>
-            <select name="source" id="source"  style="margin-top:30px">
+            <select name="source" id="source">
                 <option selected value="en">English</option>
                 <option value="vi">Vietnamese</option>
-                
             </select>
-        <input type="image" src="./img/swap.gif" id="SwapButton" onclick="swap();return false;"></input>
+        <input type="image" src="./img/swap.gif" id="SwapButton" onclick="swap();return false;">Sang</input>
             <select name="target" id="target">
                 <option value="en">English</option>
                 <option selected value="vi">Vietnamese</option>
@@ -120,18 +89,8 @@
     </div>
   </div>
 </div>
-<script>
-    $('.btn').click(function(){
-    	if(document.getElementById("word").value !=""){
-    	$('.btn').html(' <i class="fa fa-spinner fa-pulse fa-1x fa-fw"></i> ');
-    	translate_ajax();
-    }
-    else 
-    	alert('Bạn chưa nhập văn bản cần dịch');
-    })
-</script>
 <!--FOOTER-->
-<footer class="container-fluid text-center starter-template" style="margin-top:50px">
+<footer class="container-fluid text-center starter-template" style="margin-top:120px">
         <span style="color: #084B8A"><b>Contact Me</b></br></br></span>       
         <a href="https://github.com/ThaoIE3/INT2208-7-2019" class="fa fa-github fa-3x" title="Mã nguồn ứng dụng" target="_blank"></a>
         <span style="color: #084B8A"></br>Copyright &copy; <a href="./translate.php">Nhóm 19-CNPM 2209_7</a></span>
