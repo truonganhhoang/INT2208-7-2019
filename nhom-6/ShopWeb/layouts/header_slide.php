82d92a02803a500017e3b8015c3e9b633bc55612
thanhtrunggvc@gmail.com <thanhtrunggvc@gmail.com>
2019-05-04 21:51:01 +0700

    cập nhật lại ảnh slide

--- nhom-6/ShopWeb/layouts/header_slide.php
+++ nhom-6/ShopWeb/layouts/header_slide.php
@@ -19,7 +19,7 @@
 				<div id="mover">
 					<div id="slide-1" class="slide">
 						<div class="slider-img">
-							<a href="#"><img src="public/home/images/p14.jpg"></a>
+							<a href="#"><img src="public/images/slide1.jpg"></a>
 						</div>
 						
 						<div class="slider-text">
@@ -45,7 +45,7 @@
 						</div>
 
 						<div class="slider-img">
-							<a href="#"><img src="public/home/images/p12.jpg"></a>
+							<a href="#"><img src="public/images/slide2.jpg"></a>
 						</div>
 
 						<div class="clear"></div>
@@ -62,7 +62,7 @@
 						</div>
 
 						<div class="slider-img">
-							<a href="#"><img src="public/home/images/p15.jpg"></a>
+							<a href="#"><img src="public/images/slide3.jpg"></a>
 						</div>
 
 						<div class="clear"></div>
